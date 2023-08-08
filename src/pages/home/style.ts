42ec427bc62d69f082230c5ef6d2370ce274d8a5
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-08-08 16:55:32 -0300

    :lipstick: stryled preview component

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,7 +1,7 @@
-import { styled, Text } from '@ignite-ui/react'
+import { Heading, styled, Text } from '@ignite-ui/react'
 
 export const Container = styled('div', {
-  maxWidth: 'calc(100vw - (100vw -1160px) / 2)',
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
   marginLeft: 'auto',
   height: '100vh',
   display: 'flex',
@@ -13,10 +13,23 @@ export const Hero = styled('div', {
   maxWidth: 480,
   padding: '0 $10',
 
+  [`${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
   [`${Text}`]: {
     maskType: '$2',
     color: '$gray200',
   },
 })
 
-export const Preview = styled('div', {})
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
