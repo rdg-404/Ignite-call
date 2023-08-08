118fbe696145155e8b63d18191eb5f98191497fa
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-08-08 17:54:40 -0300

    :sparkles: get form info with hookform

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,23 @@
 import { Button, TextInput } from '@ignite-ui/react'
+import { useForm } from 'react-hook-form'
 import { Form } from './style'
 import { ArrowRight } from 'phosphor-react'
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm()
+
+  async function handleClaimUsername(data: any) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
