1470e7691a83861215e407f577bc7ac5781ba3c7
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-08-14 15:26:12 -0300

    :bricks:save user cookie

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -27,6 +28,11 @@ export default async function handler(
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7days
+    path: '/',
   })
 
   return res.status(200).json(user)
