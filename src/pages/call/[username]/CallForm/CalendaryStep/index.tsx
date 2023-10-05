982a0622d282a0ca549ace549a00ecb01dd41974
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-05 13:00:51 -0300

    :sparkles:show available times on front end

--- src/pages/call/[username]/CallForm/CalendaryStep/index.tsx
+++ src/pages/call/[username]/CallForm/CalendaryStep/index.tsx
@@ -6,17 +6,39 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './style'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
+import { api } from '@/lib/axios'
 
 export function CalendaryStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
+  const router = useRouter()
+
   const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const dayAndMonth = selectedDate
     ? dayjs(selectedDate).format(', DD[th ]MMMM')
     : null
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
